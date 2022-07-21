8a3c5577aaf5b69c58b84fc60175890e42815ccd
joice <joydingforever@gmail.com>
2022-07-21 14:07:02 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+    /*
+    first
+
+    public class Main {
+        static void myMethod() {
+            // code to be executed
+        }
+    }
+    second
+
+    public class Main {
+  static void myMethod() {
+    System.out.println("I just got executed!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+  }
+}
+ last
+    */
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+}
+
+// Idk deez nuts?
